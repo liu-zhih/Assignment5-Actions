12d33166bb6d0db489781376ea3aca0d51168763
liu-zhih <liuzhih@oregonstate.edu>
2020-02-28 18:14:28 -0800

    func3 test 3 - 2 changed return value to days in task.py

--- task.py
+++ task.py
@@ -13,6 +13,7 @@ def firstrun():
 # def get_first_and_last_ele(mylist):
 #     return mylist[0], mylist[-1]
 
+
 def get_days_bet_dates(date_1, date_2):
     result = date_1 - date_2
     return result.days
