b2f1439d0d617e9a1bafd100e33b212b11851992
liu-zhih <liuzhih@oregonstate.edu>
2020-02-28 18:21:18 -0800

    final test 1

--- task.py
+++ task.py
@@ -1,16 +1,16 @@
-# import math
+import math
 
 
 def firstrun():
     return "success"
 
 
-# def get_area(radius):
-#     return round(radius*radius*math.pi)
+def get_area(radius):
+    return round(radius*radius*math.pi)
 
 
-# def get_first_and_last_ele(mylist):
-#     return mylist[0], mylist[-1]
+def get_first_and_last_ele(mylist):
+    return mylist[0], mylist[-1]
 
 
 def get_days_bet_dates(date_1, date_2):
